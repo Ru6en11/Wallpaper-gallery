d392e79f6cc2cdfcc568bfac43bbb208ce27dab7
Ru6en11 <krasnoschek003@gmail.com>
2022-07-13 19:41:35 +1000

    add: toolbar and ClickListener for it, changed: navigation color

--- app/src/main/java/com/example/wallpapergallery/MainActivity.kt
+++ app/src/main/java/com/example/wallpapergallery/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.wallpapergallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import com.example.wallpapergallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +12,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home ->binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        return true
     }
 }

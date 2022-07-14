4c26caca186b2c4887ea07abfcae740086f326f4
Ru6en11 <krasnoschek003@gmail.com>
2022-07-14 13:21:54 +1000

    add: menu for toolbar

--- app/src/main/java/com/example/wallpapergallery/MainActivity.kt
+++ app/src/main/java/com/example/wallpapergallery/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.wallpapergallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.core.view.GravityCompat
 import com.example.wallpapergallery.databinding.ActivityMainBinding
@@ -18,9 +19,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
-            android.R.id.home ->binding.drawerLayout.openDrawer(GravityCompat.START)
+            android.R.id.home -> binding.drawerLayout.openDrawer(GravityCompat.START)
         }
 
         return true
@@ -28,6 +34,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun initToolbar() = with(binding) {
         setSupportActionBar(toolbar)
+
         toolbar.setTitleTextAppearance(this@MainActivity, R.style.toolbarStyle)
     }
 }

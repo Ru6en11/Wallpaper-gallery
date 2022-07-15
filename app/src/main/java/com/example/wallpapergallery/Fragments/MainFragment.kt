899803cd712ebf07a261c84bfbc8cf1fa1ef2234
Ru6en11 <krasnoschek003@gmail.com>
2022-07-15 18:45:18 +1000

    transferring functionality to the MainFragment

---
+++ app/src/main/java/com/example/wallpapergallery/Fragments/MainFragment.kt
@@ -0,0 +1,113 @@
+package com.example.wallpapergallery.Fragments
+
+import android.app.SearchManager
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.core.view.GravityCompat
+import com.example.wallpapergallery.R
+import com.example.wallpapergallery.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        initToolbar()
+        initSidebarItemSelectedListener()
+
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    //Подключаем toolbar_menu к toolbar
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar_menu, menu)
+
+        //Подключаем searchView
+        val manager = activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchItem = menu.findItem(R.id.search)
+        val searchView = searchItem?.actionView as SearchView
+
+        searchView.setSearchableInfo(manager.getSearchableInfo(activity?.componentName))
+
+        //Слушатель для searchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                //todo
+                Toast.makeText(activity as AppCompatActivity, query, Toast.LENGTH_SHORT).show()
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //todo
+                return false
+            }
+
+        })
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+    //Обработчик нажатий на элементы toolbar
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+             binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        return true
+    }
+
+    //Инициализация toolbar
+    private fun initToolbar() = with(binding) {
+        //Подключаем кастомный toolbar
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        //Смена шрифта у tittle
+        toolbar.setTitleTextAppearance(activity as AppCompatActivity, R.style.toolbarStyle)
+    }
+
+
+    private fun initSidebarItemSelectedListener() = with(binding) {
+        sidebar.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> drawerLayout.closeDrawer((GravityCompat.START))
+                R.id.favourite -> {
+                    //todo
+                }
+                R.id.history -> {
+                    //todo
+                }
+                R.id.settings -> {
+                    //todo
+                }
+                R.id.about_sidebar -> {
+                    //todo
+                }
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+
+            true
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}
--- app/src/main/java/com/example/wallpapergallery/MainActivity.kt
+++ app/src/main/java/com/example/wallpapergallery/MainActivity.kt
@@ -9,6 +9,7 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.GravityCompat
+import com.example.wallpapergallery.Fragments.MainFragment
 import com.example.wallpapergallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,81 +20,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
-        initToolbar()
-        initSidebarItemSelectedListener()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.holder, MainFragment.newInstance()).commit()
 
     }
 
-    //Подключаем toolbar_menu к toolbar
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.toolbar_menu, menu)
-
-        //Подключаем searchView
-        val manager =getSystemService(Context.SEARCH_SERVICE) as SearchManager
-        val searchItem = menu?.findItem(R.id.search)
-        val searchView = searchItem?.actionView as SearchView
-
-        searchView.setSearchableInfo(manager.getSearchableInfo(componentName))
-
-        //Слушатель для searchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                //todo
-                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                //todo
-                return false
-            }
-
-        })
-
-        return true
-    }
-
-    //Обработчик нажатий на элементы toolbar
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            android.R.id.home -> binding.drawerLayout.openDrawer(GravityCompat.START)
-            R.id.search -> {
-                //todo
-                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        return true
-    }
-
-    //Инициализация toolbar
-    private fun initToolbar() = with(binding) {
-        //Подключаем кастомный toolbar
-        setSupportActionBar(toolbar)
-        //Смена шрифта у tittle
-        toolbar.setTitleTextAppearance(this@MainActivity, R.style.toolbarStyle)
-    }
-
-    private fun initSidebarItemSelectedListener() = with(binding) {
-        sidebar.setNavigationItemSelectedListener {
-            when(it.itemId) {
-                R.id.home -> drawerLayout.closeDrawer((GravityCompat.START))
-                R.id.favourite -> {
-                    //todo
-                }
-                R.id.history -> {
-                    //todo
-                }
-                R.id.settings -> {
-                    //todo
-                }
-                R.id.about_sidebar -> {
-                    //todo
-                }
-            }
-            drawerLayout.closeDrawer(GravityCompat.START)
-
-            true
-        }
-    }
 }

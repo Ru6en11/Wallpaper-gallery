07dc2e552aad1c3590087eddded6b566d1685a8e
Ru6en11 <krasnoschek003@gmail.com>
2022-07-17 19:53:06 +1000

    fix: clear cash on finish app

--- app/src/main/java/com/example/wallpapergallery/MainActivity.kt
+++ app/src/main/java/com/example/wallpapergallery/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.wallpapergallery
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.wallpapergallery.fragments.MainFragment
 import com.example.wallpapergallery.databinding.ActivityMainBinding
 import java.io.File
 import java.lang.Exception
 import kotlin.concurrent.thread
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private var backPressed: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +26,19 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-//        deleteCache(this)
+    override fun onBackPressed() {
+        if (backPressed + 2000 > System.currentTimeMillis()) {
+            println("OnBackPressed")
+            deleteCache(this)
+            super.onBackPressed()
+        } else {
+            Toast.makeText(this, "Press once again to exits", Toast.LENGTH_SHORT).show()
+        }
+        backPressed = System.currentTimeMillis()
+
     }
+
+
 
     private fun deleteCache(context: Context) {
         try {

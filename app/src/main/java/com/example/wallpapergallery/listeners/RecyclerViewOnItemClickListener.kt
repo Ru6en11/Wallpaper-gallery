120e4cf9bb253ebd45bcd643b1deb75c09a84fda
Ru6en11 <krasnoschek003@gmail.com>
2022-07-25 09:29:35 +1000

    create onClick function on interface

--- app/src/main/java/com/example/wallpapergallery/listeners/RecyclerViewOnItemClickListener.kt
+++ app/src/main/java/com/example/wallpapergallery/listeners/RecyclerViewOnItemClickListener.kt
@@ -1,4 +1,8 @@
 package com.example.wallpapergallery.listeners
 
+import com.example.wallpapergallery.models.WallpaperModel
+
 interface RecyclerViewOnItemClickListener {
+
+    fun onClickRecyclerViewItem(wallpaper: WallpaperModel)
 }
